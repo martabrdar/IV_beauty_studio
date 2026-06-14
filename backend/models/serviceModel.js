f8992a6787936fa8de7b5b7f6547f0c6ba3bc088
martabrdar <marta.brdar06@gmail.com>
2026-06-15 01:30:54 +0200

    ispravka

--- backend/models/serviceModel.js
+++ backend/models/serviceModel.js
@@ -5,8 +5,8 @@ const serviceSchema = mongoose.Schema(
         name: { type: String, required: true },
         description: { type: String, required: true },
         price: { type: Number, required: true },
-        duration: { type: Number, required: true }, // u minutima
-        category: { type: String, required: true }, // npr. "Manikir", "Pedikir", "Trepavice"
+        duration: { type: Number, required: true }, 
+        category: { type: String, required: true }, 
         image: { type: String },
     },
     { timestamps: true }
