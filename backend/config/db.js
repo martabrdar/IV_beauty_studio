4646de488cf722379d65436e09ca04588ad9d240
martabrdar <marta.brdar06@gmail.com>
2026-06-17 11:59:47 +0200

    Povezana Atlas baza, finalne ispravke

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect("mongodb://localhost:27017/ivbeautystudio");
+        const conn = await mongoose.connect("mongodb+srv://martabrdar:maksLaks123@cluster0.nrqwnet.mongodb.net/ivbeautystudio?appName=Cluster0");
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
